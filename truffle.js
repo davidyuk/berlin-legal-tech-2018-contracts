78e4ff9aab188ad9a361c47d8da3e14691387955
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2018-02-23 13:05:22 +0100

    Add migrate-kovan script

--- truffle.js
+++ truffle.js
@@ -1,3 +1,6 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = process.env.MNEMONIC;
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -7,6 +10,12 @@ module.exports = {
       port: 8545,
       network_id: "*",
       gas: 4700000
+    },
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://kovan.infura.io/")
+      },
+      network_id: 42,
     }
   }
 };
